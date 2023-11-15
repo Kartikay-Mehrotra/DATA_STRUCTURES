26ebea43542caf5e97f53dca55dc4d128c03c814
Kartikay Mehrotra <74923561+Kartikay-Mehrotra@users.noreply.github.com>
2023-11-15 15:51:20 +0530

    Update insertionSort.cpp

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -5,15 +5,17 @@ using namespace std;
 
 void insertionSort(vector<int>& nums)
 {
-    for(int i=0;i<=nums.size()-1;i++)
-    {
-        int j=i;
-        while(j>0 && nums[j-1]>nums[j])
-        {
-            swap(nums[j-1],nums[j]);
-            j--;
-        }
+   for (int step = 1; step < size; step++) {
+    int key = array[step];
+    int j = step - 1;
+
+  
+    while (key < array[j] && j >= 0) {
+      array[j + 1] = array[j];
+      --j;
     }
+    array[j + 1] = key;
+  }
 }
 
 
